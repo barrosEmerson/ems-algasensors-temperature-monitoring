7564b3b4f0046d01688d15165341691451f6b966
Emerson Barros <emerson.barrostech@hotmail.com>
2025-04-15 20:22:41 -0300

    first commit

---
+++ src/main/java/com/barrostech/algasensors/tempature_monitoring/TempatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.barrostech.algasensors.tempature_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempatureMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barrostech/algasensors/tempature_monitoring/TempatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barrostech.algasensors.tempature_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
